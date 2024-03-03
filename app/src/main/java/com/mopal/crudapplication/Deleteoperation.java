9c3a1f9e1affdef7d7f1464ac22d76c52b73b38b
Carl Jay Margate <76047427+Iwannalearntoprogram@users.noreply.github.com>
2024-03-04 06:37:39 +0800

    need to be compiled with other CRUD operations

---
+++ app/src/main/java/com/mopal/crudapplication/Deleteoperation.java
@@ -0,0 +1,29 @@
+package com.example.test;
+
+import android.view.View;
+import android.widget.Button;
+
+public class Deleteoperation {
+
+
+    Button btndelete =findViewById(R.id.DELETE);
+
+
+    btndelete.setOnClickListener( new View.OnClickListener()){
+
+        public void onClick(View v){
+            Connection connection = connectionclass();
+            try{
+                if (connection != null) {
+                    String sqldelete ="Delete Userinfo" + id.getText().toString() +"'";
+                    Statement st = connection.createStatement();
+                    ResultSet rs = st.executeQuery(sqldelete);
+                }
+
+            }catch (Exception exception){
+                Log.e("Error", exception.getMessage());
+            }
+        }
+    }
+
+}
