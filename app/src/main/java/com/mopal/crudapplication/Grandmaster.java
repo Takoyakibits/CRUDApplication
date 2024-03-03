75a38e10604202742496b66cf5dadffca494266b
Carl Jay Margate <76047427+Iwannalearntoprogram@users.noreply.github.com>
2024-03-04 01:44:58 +0800

    Update Grandmaster.java

--- app/src/main/java/com/mopal/crudapplication/Grandmaster.java
+++ app/src/main/java/com/mopal/crudapplication/Grandmaster.java
@@ -2,16 +2,56 @@ package com.mopal.crudapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Grandmaster extends AppCompatActivity {
     private Button sovereign;
     private Button challenger;
     private Button grandmaster;
+
+    private Button guild;
+    private Button lanes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grandmaster);
+
+        challenger = findViewById(R.id.CHALLENGER);
+        challenger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Grandmaster.this, Challenger.class);
+                startActivity(intent);
+            }
+        });
+        sovereign = findViewById(R.id.SOVEREIGN);
+        sovereign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Grandmaster.this, leaderboard.class);
+                startActivity(intent);
+            }
+        });
+        guild = findViewById(R.id.GUILD);
+        guild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Grandmaster.this, Guild.class);
+                startActivity(intent);
+            }
+        });
+
+        lanes = findViewById(R.id.LANES);
+        lanes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Grandmaster.this, Toplane.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
