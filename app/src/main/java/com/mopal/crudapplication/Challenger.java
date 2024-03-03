65ccd4eb7b6a4e9841b333cc22093a36b22515be
Carl Jay Margate <76047427+Iwannalearntoprogram@users.noreply.github.com>
2024-03-04 01:44:04 +0800

    Update Challenger.java
    added some functions

--- app/src/main/java/com/mopal/crudapplication/Challenger.java
+++ app/src/main/java/com/mopal/crudapplication/Challenger.java
@@ -2,16 +2,57 @@ package com.mopal.crudapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Challenger extends AppCompatActivity {
     private Button sovereign;
     private Button challenger;
     private Button grandmaster;
+
+    private Button guild;
+    private Button lanes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenger);
+
+
+        sovereign = findViewById(R.id.SOVEREIGN);
+        sovereign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Challenger.this, leaderboard.class);
+                startActivity(intent);
+            }
+        });
+        grandmaster = findViewById(R.id.GRANDMASTER);
+        grandmaster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Challenger.this, Grandmaster.class);
+                startActivity(intent);
+            }
+        });
+        guild = findViewById(R.id.GUILD);
+        guild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Challenger.this, Guild.class);
+                startActivity(intent);
+            }
+        });
+
+        lanes = findViewById(R.id.LANES);
+        lanes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Challenger.this, Toplane.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
